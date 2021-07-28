5b51f3dc22035c4272756e872139d1d228108bbb
Code-Angler <xkqkrtk@naver.com>
2021-07-29 00:55:31 +0900

    fix: Dto 이름 변경 [#5]
    NoteCreateRequestDto 를 NoteRequestDto로 변경

--- src/main/java/com/example/teampandanback/dto/note/NoteRequestDto.java
+++ src/main/java/com/example/teampandanback/dto/note/NoteRequestDto.java
@@ -3,7 +3,7 @@ package com.example.teampandanback.dto.note;
 import lombok.Getter;
 
 @Getter
-public class NoteCreateRequestDto {
+public class NoteRequestDto {
     private String title;
     private String content;
     private String deadline;
