28bcaf4c1d50374df2654e94881849916abdaae6
BlossomWhale <85476751+BlossomWhale@users.noreply.github.com>
2021-07-28 17:07:44 +0900

    feat: 유저-프로젝트 매핑 테이블을 생성

--- src/main/java/com/example/teampandanback/domain/user_project_connect/UserProjectConnectRepository.java
+++
@@ -1,6 +0,0 @@
-//package com.example.teampandanback.domain.user_project_connect;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserProjectConnectRepository extends JpaRepository<UserProjectConnect, Long> {
-//}
