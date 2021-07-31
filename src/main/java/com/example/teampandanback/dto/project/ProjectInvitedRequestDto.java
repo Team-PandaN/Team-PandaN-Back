47cd7f3a73a002de71d1a5a9685a671cc051aa03
BlossomWhale <85476751+BlossomWhale@users.noreply.github.com>
2021-07-31 11:42:03 +0900

    fix: Dto 위변조 가능성 핸들링
    Co-authored-by: Seungyeon Kang <tmddusgood@gmail.com>

--- src/main/java/com/example/teampandanback/dto/project/ProjectInvitedRequestDto.java
+++ src/main/java/com/example/teampandanback/dto/project/ProjectInvitedRequestDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class ProjectInvitedRequestDto {
-    private String inviteCode;
+    private final String inviteCode;
 
     @Builder
     public ProjectInvitedRequestDto(String inviteCode) {
