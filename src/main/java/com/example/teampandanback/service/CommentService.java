c145ac34e89668e640b985f485e30c27d91e01c2
BlossomWhale <85476751+BlossomWhale@users.noreply.github.com>
2021-08-11 21:40:32 +0900

    fix : Comment create, 프로젝트에 속해있지 않던 유저가 해당 노트에 댓글을 다는 행위 제한

--- src/main/java/com/example/teampandanback/service/CommentService.java
+++ src/main/java/com/example/teampandanback/service/CommentService.java
@@ -5,8 +5,11 @@ import com.example.teampandanback.domain.Comment.Comment;
 import com.example.teampandanback.domain.Comment.CommentRepository;
 import com.example.teampandanback.domain.note.Note;
 import com.example.teampandanback.domain.note.NoteRepository;
+import com.example.teampandanback.domain.project.Project;
 import com.example.teampandanback.domain.user.User;
 import com.example.teampandanback.domain.user.UserRepository;
+import com.example.teampandanback.domain.user_project_mapping.UserProjectMapping;
+import com.example.teampandanback.domain.user_project_mapping.UserProjectMappingRepository;
 import com.example.teampandanback.dto.auth.SessionUser;
 import com.example.teampandanback.dto.comment.request.CommentCreateRequestDto;
 import com.example.teampandanback.dto.comment.response.CommentCreateResponseDto;
@@ -23,6 +26,7 @@ public class CommentService {
     private final UserRepository userRepository;
     private final NoteRepository noteRepository;
     private final CommentRepository commentRepository;
+    private final UserProjectMappingRepository userProjectMappingRepository;
 
     public CommentCreateResponseDto createComment(Long noteId, SessionUser sessionUser, CommentCreateRequestDto commentCreateRequestDto){
         User user = userRepository.findById(sessionUser.getUserId()).orElseThrow(
@@ -32,6 +36,17 @@ public class CommentService {
         Note note = noteRepository.findById(noteId).orElseThrow(
                 () -> new ApiRequestException("생성되지 않은 노트입니다.")
         );
+
+        //쿼리
+        Project connectedProject = Optional.ofNullable(note.getProject()).orElseThrow(
+                () -> new ApiRequestException("연결된 프로젝트가 없습니다.")
+        );
+
+        //쿼리
+        UserProjectMapping userProjectMapping = userProjectMappingRepository.findByUserAndProject(user, connectedProject)
+                .orElseThrow(
+                        () -> new ApiRequestException("user와 project mapping을 찾지 못했습니다.")
+                );
 
         Comment newComment = Comment.builder()
                 .user(user)
