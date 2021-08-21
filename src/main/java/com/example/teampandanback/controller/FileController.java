aab7e70662b726406ebd2554c61f0b53cc0096bf
Code-Angler <xkqkrtk@naver.com>
2021-08-21 21:49:26 +0900

    refactoring: 파일만 생성 API 관련 파일 제거

--- src/main/java/com/example/teampandanback/controller/FileController.java
+++ src/main/java/com/example/teampandanback/controller/FileController.java
@@ -2,15 +2,15 @@ package com.example.teampandanback.controller;
 
 
 import com.example.teampandanback.OAuth2.UserDetailsImpl;
-import com.example.teampandanback.dto.file.request.FileCreateRequestDto;
-import com.example.teampandanback.dto.file.response.FileCreateResponseDto;
 import com.example.teampandanback.dto.file.response.FileDeleteResponseDto;
 import com.example.teampandanback.service.FileService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = {"파일"})
 @RequiredArgsConstructor
@@ -18,13 +18,6 @@ import org.springframework.web.bind.annotation.*;
 public class FileController {
 
     private final FileService fileService;
-
-    @ApiOperation(value = "파일 작성")
-    @PostMapping("/api/files/{noteId}")
-    public FileCreateResponseDto createFile(
-            @PathVariable Long noteId, @AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody FileCreateRequestDto fileCreateRequestDto) {
-        return fileService.createFile(noteId, userDetails.getUser(), fileCreateRequestDto);
-    }
 
     @ApiOperation(value = "파일 삭제")
     @DeleteMapping("/api/files/{fileId}")
--- src/main/java/com/example/teampandanback/service/FileService.java
+++ src/main/java/com/example/teampandanback/service/FileService.java
@@ -2,76 +2,24 @@ package com.example.teampandanback.service;
 
 import com.example.teampandanback.domain.file.File;
 import com.example.teampandanback.domain.file.FileRepository;
-import com.example.teampandanback.domain.note.Note;
-import com.example.teampandanback.domain.note.NoteRepository;
-import com.example.teampandanback.domain.project.Project;
 import com.example.teampandanback.domain.user.User;
-import com.example.teampandanback.domain.user.UserRepository;
 import com.example.teampandanback.domain.user_project_mapping.UserProjectMapping;
 import com.example.teampandanback.domain.user_project_mapping.UserProjectMappingRepository;
-import com.example.teampandanback.dto.file.request.FileCreateRequestDto;
-import com.example.teampandanback.dto.file.request.FileDetailRequestDto;
-import com.example.teampandanback.dto.file.response.FileCreateResponseDto;
 import com.example.teampandanback.dto.file.response.FileDeleteResponseDto;
-import com.example.teampandanback.dto.file.response.FileDetailResponseDto;
 import com.example.teampandanback.exception.ApiRequestException;
-import com.example.teampandanback.utils.PandanUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class FileService {
 
-    private final UserRepository userRepository;
-    private final NoteRepository noteRepository;
     private final FileRepository fileRepository;
     private final UserProjectMappingRepository userProjectMappingRepository;
-    private final PandanUtils pandanUtils;
-
-    @Transactional
-    public FileCreateResponseDto createFile(Long noteId, User currentUser, FileCreateRequestDto fileCreateRequestDto) {
-        User user = userRepository.findById(currentUser.getUserId()).orElseThrow(
-                () -> new ApiRequestException("등록되지 않은 유저의 접근입니다.")
-        );
-
-        Note note = noteRepository.findById(noteId).orElseThrow(
-                () -> new ApiRequestException("생성되지 않은 노트입니다.")
-        );
-
-        Project connectedProject = Optional.ofNullable(note.getProject()).orElseThrow(
-                () -> new ApiRequestException("연결된 프로젝트가 없습니다.")
-        );
-        UserProjectMapping userProjectMapping = userProjectMappingRepository.findByUserAndProject(user, connectedProject)
-                .orElseThrow(
-                        () -> new ApiRequestException("user와 project mapping을 찾지 못했습니다.")
-                );
-
-        List<FileDetailRequestDto> files = new ArrayList<>(fileCreateRequestDto.getFiles());
-        files.stream()
-                .map(file -> new File(file.getFileName(), file.getFileUrl(), user, note))
-                .forEach(fileRepository::save);
-
-        List<File> fileList = fileRepository.findFilesByNoteId(noteId);
-        if (fileList.size() > pandanUtils.limitOfFile()) {
-            throw new ApiRequestException(pandanUtils.messageForLimitOfFile());
-        }
-
-        List<FileDetailResponseDto> fileDetailResponseDtoList = new ArrayList<>();
-        for (File file : fileList) {
-            fileDetailResponseDtoList.add(FileDetailResponseDto.fromEntity(file));
-        }
-
-        return FileCreateResponseDto.builder()
-                .files(fileDetailResponseDtoList)
-                .build();
-
-    }
 
     @Transactional
     public FileDeleteResponseDto deleteFile(Long fileId, User currentUser) {
