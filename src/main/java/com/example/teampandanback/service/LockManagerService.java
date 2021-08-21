a47bbf0342366f6af6138371799c347cfe7c60d4
BlossomWhale <85476751+BlossomWhale@users.noreply.github.com>
2021-08-21 20:34:21 +0900

    feat: 문닫고 들어가기

---
+++ src/main/java/com/example/teampandanback/service/LockManagerService.java
@@ -0,0 +1,62 @@
+package com.example.teampandanback.service;
+
+import com.example.teampandanback.domain.note.Note;
+import com.example.teampandanback.domain.note.NoteRepository;
+import com.example.teampandanback.exception.ApiRequestException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class LockManagerService {
+    private final NoteRepository noteRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Transactional
+    public void preProcess(Long noteId){
+        em.clear();
+        Note note = noteRepository.findById(noteId).orElseThrow(
+                () -> new ApiRequestException("해당 노트가 없습니다.")
+        );
+        note.setLock(true);
+        note.setUsing(true);
+    }
+
+    @Transactional
+    public void deLock(Long noteId){
+        em.clear();
+        Note note = noteRepository.findById(noteId).orElseThrow(
+                ()-> new ApiRequestException("해당 노트가 없습니다.")
+        );
+        note.setLock(true);
+        note.setUsing(true);
+    }
+
+    @Transactional
+    public Boolean isAnyoneWriting(Long noteId){
+        em.clear();
+        Note note = noteRepository.findById(noteId).orElseThrow(
+                ()-> new ApiRequestException("해당 노트가 없습니다.")
+        );
+        if(note.getUsing() == true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Transactional
+    public void assumeThatNobodyIsWriting(Long noteId){
+        em.clear();
+        Note note = noteRepository.findById(noteId).orElseThrow(
+                ()-> new ApiRequestException("해당 노트가 없습니다.")
+        );
+        note.setUsing(false);
+    }
+}
